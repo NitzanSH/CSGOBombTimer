8ae0039459aa5f1ecf1f56f670c95679d39ed30c
Crapy <dordamri@gmail.com>
2015-12-19 11:30:00 +0200

    BombTimer

---
+++ CSGO_BombTimer/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CSGO_IntegrationGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CSGO_BombTimer.Properties.Resources.c4;
+            this.pictureBox1.InitialImage = global::CSGO_BombTimer.Properties.Resources.c4;
+            this.pictureBox1.Location = new System.Drawing.Point(35, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 21);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(254)))));
+            this.ClientSize = new System.Drawing.Size(74, 32);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "BombTimer";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(254)))), ((int)(((byte)(254)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CSGO_BombTimer/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Forms;
+using CSGSI;
+using Timer = System.Timers.Timer;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace CSGO_IntegrationGUI
+{
+    
+    public partial class Form1 : Form
+    {
+        public int bombTimer = 0;
+        public int BOMB_SECS = 40;
+        public Timer t = new Timer(1);
+        public int timestamp = 0;
+        public int calculateSeconds()
+
+        {
+
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Local);
+
+            DateTime dtNow = DateTime.Now;
+
+            TimeSpan result = dtNow.Subtract(dt);
+
+            int seconds = Convert.ToInt32(result.TotalSeconds);
+
+            return seconds;
+
+        }
+
+        public Form1()
+        {
+            t.Elapsed += OnTimedEvent;
+            t.AutoReset = true;
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.FormClosed += new FormClosedEventHandler(Form2_FormClosed);
+            frm.Show();
+            this.Opacity = 0;
+            //this.SetDesktopLocation(System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Right - this.Width, 100);
+
+            //subscribe to the NewGameState event
+            GSIListener.NewGameState += new EventHandler(OnNewGameState);
+
+            //start listening on http://127.0.0.1:3000/
+            if (GSIListener.Start(3000))
+            {
+                label1.Text = "Listening..";
+            }
+            else
+            {
+                //Console.WriteLine("Error starting GSIListener.");
+                label1.Text = "Error";
+            }
+        }
+
+        private void OnNewGameState(object sender, EventArgs e)
+        {
+            //the newest GameState object is provided as the sender
+            GameState gs = (GameState)sender;
+
+            GameStateNode gsRound = gs.Round;
+            GameStateNode gsProvider = gs.Provider;
+            if (gsRound.GetValue("bomb") == "planted")
+            {
+                if (bombTimer <= 1) { 
+                    timestamp = Int32.Parse(gsProvider.GetValue("timestamp"));
+                    t.Enabled = true;
+                }
+            }
+            else
+            {
+                bombTimer = 0;
+                t.Enabled = false;
+                this.Invoke((MethodInvoker)(() => this.Opacity = 0));
+            }
+
+        }
+
+        void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            bombTimer = ((timestamp + BOMB_SECS) - calculateSeconds()) + 7200;
+            if (bombTimer >= 0) {
+            
+              label1.Invoke((MethodInvoker)(() => label1.Text = bombTimer.ToString()));
+            }
+            if(GetActiveWindowTitle() == "Counter-Strike: Global Offensive")
+            {
+                this.Invoke((MethodInvoker)(() => this.Opacity = 50));
+                const int nChars = 256;
+                StringBuilder Buff = new StringBuilder(nChars);
+                IntPtr handle = GetForegroundWindow();
+                Rect CSGORect = new Rect();
+                GetWindowRect(handle, ref CSGORect);
+                this.Invoke((MethodInvoker)(() => this.SetDesktopLocation(CSGORect.Right - this.Width, CSGORect.Top + 100 + this.Height)));
+            }
+            else
+            {
+                this.Invoke((MethodInvoker)(() => this.Opacity = 0));
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            IntPtr handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        
+    }
+}
